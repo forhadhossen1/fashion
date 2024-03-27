67b2dfbd416e6b4535ba9446040905aad22b0cda
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-27 22:03:12 +0600

    setup layouts and path system

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layouts/Layout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Layout></Layout>,
+        children : [
+            {
+                path : '/',
+                element : 'hello world'
+            }
+        ]
+
     },
 ]);
 
