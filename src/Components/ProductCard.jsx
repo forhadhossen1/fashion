cae2d0e520b74376617b05983369bf6b368f1b02
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-30 23:43:58 +0600

    setup all card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,6 +1,6 @@
 
 const ProductCard = ({ product }) => {
-    const { image, offerpercentage, price, offerPrice } = product || {}
+    const { image, offerpercentage, price, offerPrice, title } = product || {}
     return (
         <div>
             <section className="mx-auto w-fit p-12">
@@ -8,14 +8,20 @@ const ProductCard = ({ product }) => {
                 <div className="w-72 h-fit group">
                     <div className="relative overflow-hidden">
                         <img className="h-96 w-full object-cover" src={image} alt="product image" />
-                        <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                            <button className="bg-black text-white py-2 px-5">Add to cart</button>
+                        <div className="absolute bg-white mt-0 p-2">{offerpercentage}%</div>
+                        <div className="flex">
+                            <div className="absolute left-0 bottom-0 bg-black/20 flex items-center justify-center group-hover:opacity-100 transition-all duration-300">
+                                <button className="bg-black text-white py-2 px-5">Add to cart</button>
+                            </div>
+                            <div className="absolute right-0 bottom-0 bg-black/20 flex items-center justify-center group-hover:opacity-100 transition-all duration-300">
+                                <button className="bg-black text-white py-2 px-5">Add to favourite</button>
+                            </div>
+                        </div>
                         </div>
                     </div>
-                    <h2 className="mt-3 text-xl capitalize">Straw Hat</h2>
-                    <del className="text-red-700 text-lg">$49</del>
-                    <p className="text-xl mt-2 ml-1 inline-block">$35</p>
-                </div>
+                    <h2 className="mt-3 text-xl capitalize">{title}</h2>
+                    <del className="text-red-700 text-lg">${price}</del>
+                    <p className="text-xl mt-2 ml-1 inline-block">${offerPrice}</p>
             </section>
         </div>
     );
