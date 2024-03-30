b79577934fea6ee26a8259fd3b605a6ecff956f9
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-30 16:17:20 +0600

    map the products and pus this props

--- src/Components/HomeComponents/BestSale.jsx
+++ src/Components/HomeComponents/BestSale.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ProductCard from "../ProductCard";
 
 const BestSale = () => {
     const [products, setProducts] = useState([]);
@@ -13,20 +14,14 @@ const BestSale = () => {
     }, [])
     return (
         <div>
-            <section className="mx-auto w-fit p-12">
-                {/* <!-- Card --> */}
-                <div className="w-72 h-fit group">
-                    <div className="relative overflow-hidden">
-                        <img className="h-96 w-full object-cover" src="https://images.unsplash.com/photo-1659576294143-1da218a2d82e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
-                        <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                            <button className="bg-black text-white py-2 px-5">Add to cart</button>
-                        </div>
-                    </div>
-                    <h2 className="mt-3 text-xl capitalize">Straw Hat</h2>
-                    <del className="text-red-700 text-lg">$49</del>
-                    <p className="text-xl mt-2 ml-1 inline-block">$35</p>
-                </div>
-            </section>
+            {
+                products.map(product => (
+                    <ProductCard
+                        key={product._id}
+                        product={product}
+                    />
+                ))
+            }
         </div>
     );
 };
