c7b75c34294724eb80c0ec0fe1f174b2054aa8a1
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-31 03:48:18 +0600

    change this footer and custozied

--- src/Components/FooterComponent.jsx
+++ src/Components/FooterComponent.jsx
@@ -1,56 +1,27 @@
 import { Footer } from "flowbite-react";
-import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 
 const FooterComponent = () => {
     return (
-        <Footer container>
-            <div className="w-full">
-                <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-                    <div>
-                        <Footer.Brand
-                            href="https://flowbite.com"
-                            src="https://flowbite.com/docs/images/logo.svg"
-                            alt="Flowbite Logo"
-                            name="Flowbite"
-                        />
-                    </div>
-                    <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-                        <div>
-                            <Footer.Title title="about" />
-                            <Footer.LinkGroup col>
-                                <Footer.Link href="#">Flowbite</Footer.Link>
-                                <Footer.Link href="#">Tailwind CSS</Footer.Link>
-                            </Footer.LinkGroup>
-                        </div>
-                        <div>
-                            <Footer.Title title="Follow us" />
-                            <Footer.LinkGroup col>
-                                <Footer.Link href="#">Github</Footer.Link>
-                                <Footer.Link href="#">Discord</Footer.Link>
-                            </Footer.LinkGroup>
-                        </div>
-                        <div>
-                            <Footer.Title title="Legal" />
-                            <Footer.LinkGroup col>
-                                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-                            </Footer.LinkGroup>
-                        </div>
-                    </div>
-                </div>
-                <Footer.Divider />
-                <div className="w-full sm:flex sm:items-center sm:justify-between">
-                    <Footer.Copyright href="#" by="Flowbite™" year={2022} />
-                    <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-                        <Footer.Icon href="#" icon={BsFacebook} />
-                        <Footer.Icon href="#" icon={BsInstagram} />
-                        <Footer.Icon href="#" icon={BsTwitter} />
-                        <Footer.Icon href="#" icon={BsGithub} />
-                        <Footer.Icon href="#" icon={BsDribbble} />
-                    </div>
-                </div>
-            </div>
-        </Footer>
+        <Footer container className="bg-slate-100 mt-12">
+        <div className="w-full text-center">
+          <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+            <Footer.Brand
+              href="#"
+              src="https://i.ibb.co/RDD7bZX/fashion-favicon.png"
+              alt="Flowbite Logo"
+              name="Fashion"
+            />
+            <Footer.LinkGroup className="text-lg font-medium text-black">
+              <Footer.Link href="#">About</Footer.Link>
+              <Footer.Link href="#">Privacy Policy</Footer.Link>
+              <Footer.Link href="#">Licensing</Footer.Link>
+              <Footer.Link href="#">Contact</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <Footer.Divider />
+          <Footer.Copyright href="#" by="Fashion™" year={2024} className="text-lg text-black font-medium" />
+        </div>
+      </Footer>
     );
 };
 
--- src/Pages/Kids/Kids.jsx
+++ src/Pages/Kids/Kids.jsx
@@ -1,15 +1,28 @@
 import { Helmet } from "react-helmet-async";
 import kidsBanner from '../../assets/Banner/kidsBanner.png'
+import useProduct from "../../Hooks/useProduct";
+import ProductCard from "../../Components/ProductCard";
 
 const Kids = () => {
+    const [product] = useProduct();
+    const products = product.filter(product => product.type === 'women')
     return (
         <div>
             <Helmet>
                 <title>Fashion | Kids</title>
             </Helmet>
-          
+
             <div className='pb-12'>
                 <img src={kidsBanner} className='w-full md:h-[70vh] rounded-lg ' alt="banner" />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+                {
+                    products.map(product => (<ProductCard
+                        key={product._id}
+                        product={product}
+                    />))
+                }
             </div>
         </div>
     );
