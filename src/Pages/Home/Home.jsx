e8816854e6e03469d34f0cfcf054ff073ca4d638
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-30 13:49:58 +0600

    create footer and home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import FooterComponent from "../../Components/FooterComponent";
 
 const Home = () => {
     return (
         <div>
             <p className="py-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet ipsam labore commodi officiis quidem doloremque nisi provident quis eos doloribus. Voluptatibus mollitia facilis quas cum sapiente soluta, porro officiis tempore.
             </p>
+            <FooterComponent />
         </div>
     );
 };
