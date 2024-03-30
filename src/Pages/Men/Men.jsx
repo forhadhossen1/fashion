68b240361887cbcac7b1ef9cab075a6c6e10d0e1
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-31 02:31:34 +0600

    fetch data for men page

--- src/Pages/Men/Men.jsx
+++ src/Pages/Men/Men.jsx
@@ -1,12 +1,24 @@
 import { Helmet } from "react-helmet-async";
+import useProduct from "../../Hooks/useProduct";
+import ProductCard from "../../Components/ProductCard";
 
 const Men = () => {
+    const [product] = useProduct();
+    const products = product.filter(product => product.type === 'men')
     return (
         <div>
             <Helmet>
                 <title>Fashion | Men</title>
             </Helmet>
-            yesh man coth page
+
+            <div>
+                {
+                    products.map(product => (<ProductCard
+                        key={product._id}
+                        product={product}
+                    />))
+                }
+            </div>
         </div>
     );
 };
