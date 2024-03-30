058894ff798eacecd7949aeb748a60c8522b7aa7
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-30 16:26:47 +0600

    change json data and showing card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,12 +1,13 @@
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { image, offerpercentage, price, offerPrice } = product || {}
     return (
         <div>
             <section className="mx-auto w-fit p-12">
                 {/* <!-- Card --> */}
                 <div className="w-72 h-fit group">
                     <div className="relative overflow-hidden">
-                        <img className="h-96 w-full object-cover" src="https://images.unsplash.com/photo-1659576294143-1da218a2d82e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
+                        <img className="h-96 w-full object-cover" src={image} alt="product image" />
                         <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
                             <button className="bg-black text-white py-2 px-5">Add to cart</button>
                         </div>
