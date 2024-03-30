1bd8d21e643108abf6e5c5e7346a1cd2c5b1090e
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-31 00:39:06 +0600

    fashion trends page ok

--- src/Components/HomeComponents/TrendFashion.jsx
+++ src/Components/HomeComponents/TrendFashion.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import trendFashion from '../../assets/Banner/FashionTrends.png'
+import ProductCard from '../ProductCard';
 const TrendFashion = () => {
     const [products, setProducts] = useState([]);
 
@@ -19,9 +20,15 @@ const TrendFashion = () => {
             <div className="my-12 w-96 pt-12 mx-auto">
                 <h1 className="text-gray-800 font-bold text-4xl border-b-4 p-3 border-black text-center">Fashion Trends </h1>
             </div>
-            {
-
-            }
+            
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+                {
+                    products.map(product => <ProductCard
+                        key={product._id}
+                        product={product}
+                    />)
+                }
+            </div>
         </div>
     );
 };
