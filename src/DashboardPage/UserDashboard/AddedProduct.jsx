2855ebbdee49895632b3e46f10a9b97e36567c61
forhadhossen1 <mr.forhad1030@gmail.com>
2024-04-03 16:39:19 +0600

    create added product page and total product and price function

--- src/DashboardPage/UserDashboard/AddedProduct.jsx
+++ src/DashboardPage/UserDashboard/AddedProduct.jsx
@@ -2,10 +2,19 @@ import useCart from "../../Hooks/useCart";
 
 const AddedProduct = () => {
     const [cart] = useCart();
+    const totalOfferPrice = cart.reduce((total, product) => {
+        return total + product.offerPrice;
+    }, 0)
+    console.log("Total offerPrice:", totalOfferPrice);
     return (
         <div>
+            <div className="flex gap-5 ">
+                <h3 className="text-3xl font-bold">Total Product : {cart.length}</h3>
+
+                <h3 className="text-3xl font-bold">Total Price : {totalOfferPrice}</h3>
+            </div>
             <div>
-                Total Product : {cart.length}
+
             </div>
         </div>
     );
