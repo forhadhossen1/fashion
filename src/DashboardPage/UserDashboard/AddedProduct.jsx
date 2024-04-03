428822c38fd7fb14d162d0020c4fc7b24fcd4b3b
forhadhossen1 <mr.forhad1030@gmail.com>
2024-04-03 16:54:39 +0600

    added added product table

--- src/DashboardPage/UserDashboard/AddedProduct.jsx
+++ src/DashboardPage/UserDashboard/AddedProduct.jsx
@@ -1,4 +1,5 @@
 import useCart from "../../Hooks/useCart";
+import { Table } from "flowbite-react";
 
 const AddedProduct = () => {
     const [cart] = useCart();
@@ -13,8 +14,31 @@ const AddedProduct = () => {
 
                 <h3 className="text-3xl font-bold">Total Price : {totalOfferPrice}</h3>
             </div>
-            <div>
 
+            <div>
+                <div className="overflow-x-auto">
+                    <Table hoverable>
+                        <Table.Head>
+                            <Table.HeadCell>Product Image</Table.HeadCell>
+                            <Table.HeadCell>Product name</Table.HeadCell>
+                            <Table.HeadCell>Size</Table.HeadCell>
+                            <Table.HeadCell>Quantity</Table.HeadCell>
+                            <Table.HeadCell>Price</Table.HeadCell>
+
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                <Table.Cell>product image</Table.Cell>
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                    {'Apple MacBook Pro 17"'}
+                                </Table.Cell>
+                                <Table.Cell>size</Table.Cell>
+                                <Table.Cell>quanlity</Table.Cell>
+                                <Table.Cell>$2999</Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                    </Table>
+                </div>
             </div>
         </div>
     );
