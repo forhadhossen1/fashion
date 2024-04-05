ee2997b1de212d75f00f5c87d3ed410c869d8f33
forhadhossen1 <mr.forhad1030@gmail.com>
2024-04-06 01:33:41 +0600

    create users post api in google login system

--- src/Components/SocialLogin/GoogleLogin.jsx
+++ src/Components/SocialLogin/GoogleLogin.jsx
@@ -1,14 +1,27 @@
 
 import { FcGoogle } from 'react-icons/fc';
 import useAuth from '../../Hooks/useAuth';
+import useAxiosPublic from '../../Hooks/useAxiosPublic'
+import { useNavigate } from "react-router-dom";
 
 const GoogleLogin = () => {
     const { googleSignIn } = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
 
     const handleGoogleSignIn = () => {
         googleSignIn()
             .then(result => {
-                console.log(result)
+                // console.log(result.user)
+                const userInfo = {
+                    name: result.user?.displayName,
+                    email: result.user?.email
+                }
+                axiosPublic.post('/users', userInfo)
+                    .then(res => {
+                        console.log(res)
+                        navigate('/')
+                    })
             })
     }
 
