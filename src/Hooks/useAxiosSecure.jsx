ddcc591e49fb3b485d4459bc0a478291da91fa2a
forhadhossen1 <mr.forhad1030@gmail.com>
2024-04-21 13:34:59 +0600

    create intaceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,6 +5,15 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        config.headers.authorization = `Bearer ${token}`
+        console.log('request stopped by interceptors')
+        return config;
+    }, function (error) {
+        return Promise.reject(error);
+    }
+    )
     return axiosSecure;
 };
 
