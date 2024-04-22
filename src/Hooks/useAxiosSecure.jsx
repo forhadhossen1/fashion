0ebd398e090224a87fd5d706356ce9eaed81b288
forhadhossen1 <mr.forhad1030@gmail.com>
2024-04-22 23:33:06 +0600

    create unauthorised access conditional dependensi

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from '../Hooks/useAuth';
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const { logOut } = useAuth();
     //request interceptor 
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
@@ -20,9 +24,13 @@ const useAxiosSecure = () => {
     //intercepts 401 and 402 status
     axiosSecure.interceptors.response.use(function (response) {
         return response;
-    }, (error) => {
-        const status  = error.response.status;
-        console.log('status error in the interceptors', status)
+    }, async (error) => {
+        const status = error.response.status;
+        console.log('status error in the interceptors', status);
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login');
+        }
         return Promise.reject(error);
     })
 
